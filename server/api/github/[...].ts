4e92f9175fc2b738f98dd36a41530f20cde3d1a0
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-12-09 11:00:26 -0700

    adding probot for webhooks

---
+++ server/api/github/[...].ts
@@ -0,0 +1,19 @@
+import { Probot, createNodeMiddleware, createProbot } from 'probot'
+
+// For more information, see https://probot.github.io/docs/development/
+export function probot(app: Probot) {
+  // Your code here
+  app.log.info('Yay, the app was loaded!')
+  app.onAny(async (context) => {
+    app.log.info(context.payload)
+  })
+}
+
+const probotApp = new Probot({
+  appId: process.env.APP_ID,
+  privateKey: process.env.PRIVATE_KEY,
+  secret: process.env.WEBHOOK_SECRET,
+  // webhookPath: "/api/webhooks",
+})
+
+export default fromNodeMiddleware(createNodeMiddleware(probot, { webhooksPath: '/api/webhooks', probot: probotApp }))
---
+++ server/plugins/05.smee.io.ts
@@ -0,0 +1,24 @@
+import consola from 'consola'
+
+export default defineNitroPlugin(async (nitroApp) => {
+  if (process.env.NODE_ENV === 'production')
+    return
+
+  const SmeeClient = await import('smee-client').then(m => m.default || m)
+
+  const smee = new SmeeClient({
+    source: 'https://smee.io/2CheYVHetZe4ROm',
+    target: 'http://localhost:3001/api/github',
+    logger: consola,
+  })
+
+  const events = smee.start()
+
+  // consola.info('Smee started forwarding https://smee.io/2CheYVHetZe4ROm to http://localhost:3000/api/webhooks ')
+
+  nitroApp.hooks.hook('close', async () => {
+    consola.info('Stopping Smee forwarding')
+    events.close()
+  })
+  // Stop forwarding events
+})
